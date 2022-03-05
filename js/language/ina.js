8e776e25f2165383e0b3c6662c840f2159b6e622
Endang Arifin <99872967+endangarifin22@users.noreply.github.com>
2022-03-05 20:56:22 +0700

    Create file language Indonesian

---
+++ js/language/ina.js
@@ -0,0 +1,64 @@
+function aboutIna() {
+  var language = {
+  "title" : "Tentang Saya",
+  "content" : "Hallo, nama saya Endang Arifin seorang programmer pemula yang belajar di usia tua.",
+  "name" : "Nama",
+  "age" : "Umur",
+  "address" : "Alamat",
+  "address-current" : "Alamat Sekarang",
+  "profession" : "Pekerjaan Sekarang",
+  "profession-content" : "Kuli Bantu Las",
+  "skill" : "Kemampuan",
+  "skill-content" : "Las Listrik, Web development, Bertani",
+  "experience" : "Pengalaman Kerja",
+  "experience-content" : "Berkebun Kopi (Lampung Barat, 2012 - Sekarang), Jual Gorengan(Makkasar 2016 - 2017), Kuli bantu las (Indonesia (pindah-pindah tempat ikut bos) 2017 - 2018, 2018 - 2019, 2022 - sekarang)",
+  "dream" : "Mimpi",
+  "dream-content" : "Hidup Dari Programing",
+  }
+  return language;
+}
+
+function heroIna() {
+  var language = {
+  "title" : "Hallo",
+  "name" : "Saya Endang",
+  "description" : "Jika anda butuh seorang web development dengan gajih murah saya bersedia",
+  "button" : "Pekerjakan Saya",
+  }
+  return language;
+}
+
+function skillIna() {
+  var language = {
+  "title" : "Kemampuan Web Development",
+  "name-1" : "Desain Web",
+  "description-1" : "Membuat desain website ",
+  "name-2" : "Frontend Developer",
+  "description-2" : "Menerjemahkan UI/UX untuk website",
+  "name-3" : "Backend Developer",
+  "description-3" : "Membuat CRUD, API",
+
+  }
+  return language;
+}
+
+function galeryIna() {
+  var language = {
+  "title" : "Galeryku",
+  
+  }
+  return language;
+}
+
+function contactIna() {
+  var language = {
+  "title" : "Kontak",
+  "name" : "Nama",
+  "email" : "Email",
+  "message" : "Pesan",
+  "button" : "Kirim Pesan",
+  "sosmed" : "Sosial Media",
+  
+  }
+  return language;
+}
