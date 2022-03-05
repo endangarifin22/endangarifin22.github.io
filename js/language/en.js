62dd05358f5303e789172a606ea43e35bb6a760b
Endang Arifin <99872967+endangarifin22@users.noreply.github.com>
2022-03-05 20:55:27 +0700

    Create language English file

---
+++ js/language/en.js
@@ -0,0 +1,64 @@
+function aboutEn() {
+  var language = {
+  "title" : "About Me",
+  "content" : "Hello, my name is Endang Arifin, a beginner programmer who is studying at an old age.",
+  "name" : "Name",
+  "age" : "Age",
+  "address" : "Address",
+  "address-current" : "Address Current",
+  "profession" : "Profession Current",
+  "profession-content" : "Helper Welding ",
+  "skill" : "Skill",
+  "skill-content" : "Welding, Web development, Farming",
+  "experience" : "Experience",
+  "experience-content" : "Coffee Gardening(Lampung Barat, 2012 - current), Selling Fried Food(Makassar, 2016 - 2017 (10 moth)), Helper Welding(Indonesia (Moving places) (2017-2018, 2018 - 2019, 2022-current)",
+  "dream" : "Dream",
+  "dream-content" : "Can live from working as a programmer",
+  }
+  return language;
+}
+
+function heroEn() {
+  var language = {
+  "title" : "Hello",
+  "name" : "I'M Endang",
+  "description" : "If you need a web development with cheap salary I'm ready",
+  "button" : "Hire Me",
+  }
+  return language;
+}
+
+function skillEn() {
+  var language = {
+  "title" : "Web Development Capabilities",
+  "name-1" : "Web Design",
+  "description-1" : "Make a website design ",
+  "name-2" : "Frontend Developer",
+  "description-2" : "Translating UI/UX for websites",
+  "name-3" : "Backend Developer",
+  "description-3" : "Make a CRUD, API",
+
+  }
+  return language;
+}
+
+function galeryEn() {
+  var language = {
+  "title" : "My Galery",
+  
+  }
+  return language;
+}
+
+function contactEn() {
+  var language = {
+  "title" : "Contact",
+  "name" : "Name",
+  "email" : "Email",
+  "message" : "Message",
+  "button" : "Submit",
+  "sosmed" : "Sosial Media",
+  
+  }
+  return language;
+}
